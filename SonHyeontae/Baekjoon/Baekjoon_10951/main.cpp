8c0e8340efb6998b014b96249bb5d290ed613a83
Franordin <98395260+Franordin@users.noreply.github.com>
2025-04-03 22:43:56 +0900

    add

---
+++ SonHyeontae/Baekjoon/Baekjoon_10951/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	int a, b;
+
+	//while (true) {
+	//	std::cin >> a >> b;
+	//	if (std::cin.eof()) break;
+	//	std::cout << a + b << '\n';
+	//}
+
+	while (std::cin >> a >> b) {
+		std::cout << a + b << "\n";
+	}
+
+	return 0;
+}
