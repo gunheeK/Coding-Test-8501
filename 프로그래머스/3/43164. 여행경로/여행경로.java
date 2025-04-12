74713c67aab2374fa2280bad83391c290798862f
gunheeK <86161337+gunheeK@users.noreply.github.com>
2025-04-12 17:59:39 +0900

    [level 3] Title: 여행경로, Time: 43.81 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -6,23 +6,21 @@ class Solution {
         List<String> path = new ArrayList<>();
         Arrays.sort(tickets, (a, b) -> a[0].equals(b[0]) ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
         path.add("ICN");
-        dfs("ICN", tickets, visited, path);
+        dfs("ICN", visited, path, tickets);
         return answer.toArray(new String[0]);
     }
-    
-    public void dfs(String current, String[][] tickets, boolean[] visited, List<String> path) {
+    public void dfs(String current, boolean[] visited, List<String> path, String[][] tickets) {
         if(path.size() == tickets.length + 1) {
             if(answer == null) {
                 answer = new ArrayList<>(path);
             }
-            return;
         }
         for(int i = 0; i < tickets.length; i++) {
             if(!visited[i] && tickets[i][0].equals(current)) {
                 visited[i] = true;
                 path.add(tickets[i][1]);
                 
-                dfs(tickets[i][1], tickets, visited, path);
+                dfs(tickets[i][1], visited, path, tickets);
                 
                 visited[i] = false;
                 path.remove(path.size() - 1);
